add77ad743b9d2f09c71c9273678aca30237c587
Sergei Volovik <sergei.volovik@icloud.com>
2023-05-17 12:57:58 +0300

    Add code

--- TaskTwo/Program.cs
+++ TaskTwo/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an integer in decimal:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the base of the new number system (from 2 to 20):");
+        int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+        string convertedNumber = ConvertToBase(number, baseNumber);
+        Console.WriteLine($"Conversion result: {convertedNumber}");
+    }
+
+    static string ConvertToBase(int number, int baseNumber)
+    {
+        const string digits = "0123456789ABCDEFGHIJ";
+
+        if (baseNumber < 2 || baseNumber > 20)
+        {
+            Console.WriteLine("The base of the number system must be between 2 and 20.");
+            return string.Empty;
+        }
+
+        if (number == 0)
+        {
+            return "0";
+        }
+
+        string result = string.Empty;
+
+        while (number > 0)
+        {
+            int remainder = number % baseNumber;
+            result = digits[remainder] + result;
+            number /= baseNumber;
+        }
+
+        return result;
+    }
+}
